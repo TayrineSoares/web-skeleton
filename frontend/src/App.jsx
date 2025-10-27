d9cc75dcfce67eac076298f2a7944185edc7ef03
TayrineSoares <tayrinecristina@hotmail.com>
2025-10-27 18:05:26 -0400

    add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/App.css'
+import AppRoutes from './AppRoutes'
 
 function App() {
 
   return (
-    <>
+    <div>
     <h1>Inside App.jsx</h1>
+    <AppRoutes />
 
-    </>
+    </div>
   )
 }
 
